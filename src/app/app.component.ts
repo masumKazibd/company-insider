7f70d8e7dbd370eb8c0764f2ada796658876ac71
masumkazibd <masum.kazi.hantmk@gmail.com>
2026-08-13 20:19:54 +0600

    added router outlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core'; 
-import { HomeComponent } from './features/home/home/home.component';
+import { Component } from '@angular/core';  
+import { RouterOutlet } from "@angular/router";
+
 import { NavbarComponent } from './layout/navbar/navbar.component';
 import { FooterComponent } from './layout/footer/footer.component';
 @Component({
   selector: 'app-root',
-  imports: [ 
+  imports: [
     NavbarComponent,
-    HomeComponent,
-    FooterComponent
-  ],
+    FooterComponent,
+    RouterOutlet
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
